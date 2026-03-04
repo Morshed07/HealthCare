275e57d096cf64d0a06785719a34547b3b116896
Morshed07 <moshednayeem@icloud.com>
2026-03-04 23:31:57 +0600

    remove media, .env, db.sqlite3

--- reactides/settings.py
+++ reactides/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # whitenoise for serving static files in production
     'django.contrib.sessions.middleware.SessionMiddleware',
