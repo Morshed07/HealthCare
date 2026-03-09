f2825425487c2b9cec2808e9c32bf32db3db82eb
Morshed Nayeem <abdullahmorshed@joinventureai.com>
2026-03-09 19:17:55 +0600

    update settings for frontend headers

--- reactides/settings.py
+++ reactides/settings.py
@@ -35,6 +35,13 @@ CORS_ALLOWED_ORIGINS = env(
     'CORS_ALLOWED_ORIGINS',
 ).split(',')
 
+from corsheaders.defaults import default_headers
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "ngrok-skip-browser-warning",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
